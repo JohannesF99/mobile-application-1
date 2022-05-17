9656ab9716d1cfa8ae62782b8eced6453c6236ff
Johannes Franke <jofra@hotmail.de>
2022-05-17 13:38:16 +0200

    -disabled button, if textfield is empty

--- lib/screen/CreateScreen.dart
+++ lib/screen/CreateScreen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 import '../api/BackendAPI.dart';
@@ -15,6 +14,17 @@ class CreateScreen extends StatefulWidget {
 class _CreateScreenState extends State<CreateScreen>  {
 
   final _createContentController = TextEditingController();
+  bool submit = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _createContentController.addListener(() {
+      setState(() {
+        submit = _createContentController.text.isNotEmpty;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +37,7 @@ class _CreateScreenState extends State<CreateScreen>  {
             width: MediaQuery.of(context).size.width - 100,
             child: TextFormField(
               controller: _createContentController,
+              minLines: 2,
               maxLines: null,
               keyboardType: TextInputType.multiline,
               decoration: const InputDecoration(
@@ -40,11 +51,7 @@ class _CreateScreenState extends State<CreateScreen>  {
           ),
           Center(
             child: ElevatedButton(
-                onPressed: () async {
-                  final content = ContentData.withoutId(_createContentController.text);
-                  final result = await _createPost(content);
-                  Navigator.pop(context, result);
-                },
+                onPressed: submit ? () => _sendText() : null,
                 child: const Text("Send")
             ),
           )
@@ -57,5 +64,11 @@ class _CreateScreenState extends State<CreateScreen>  {
     final token = await StorageManager.readData("token");
     final username = await StorageManager.readData("username");
     return await BackendAPI().sendNewPost(token, username, content);
+  }
+
+  Future<void> _sendText() async {
+    final content = ContentData.withoutId(_createContentController.text);
+    final result = await _createPost(content);
+    Navigator.pop(context, result);
   }
 }
